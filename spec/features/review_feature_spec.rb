24f2dbb33d91efe99c72c01970f2bfc5f198da16
leopoldkwok <leopoldkwok@gmail.com>
2014-10-01 12:37:34 +0100

    added ratings

---
+++ spec/features/review_feature_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+describe 'reviewing' do
+	before do
+		Restaurant.create(name: 'KFC') # it will create a new instance of the restaurant and set the name to KFC
+	end
+
+	def leave_review(thoughts, rating)
+	# it 'allows users to leave a review using a form' do
+		visit '/restaurants'
+		click_link 'Review KFC'
+		fill_in "Thoughts", with: thoughts
+		select rating, from: 'Rating'
+		click_button 'Leave Review'
+
+		# expect(current_path).to eq '/restaurants'
+		# expect(page).to have_content('so so')
+	end
+
+	it "allows users to leave review using the form which appear alongside restaurant" do
+		leave_review("So so","3")
+		expect(current_path).to eq '/restaurants'
+		expect(page).to have_content 'So so'
+	end
+
+	it "displays an average rating for all reviews" do 
+		leave_review("So so", "3")
+		leave_review('Great', "5")
+		expect(page).to have_content("Average rating: ★★★★☆")
+	end
+
+
+end
