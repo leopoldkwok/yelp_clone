a91f21c7c389858b6d0c14defb8e8c8afbbfda3a
leopoldkwok <leopoldkwok@gmail.com>
2014-10-01 21:28:38 +0100

    added endorsements

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,6 @@
 class Review < ActiveRecord::Base
 	has_many :reviews, dependent: :destroy
 		validates :rating, :inclusion => {:in => [1,2,3,4,5]}
-
+		belongs_to :restaurant
+		has_many :endorsements
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,11 @@ Rails.application.routes.draw do
   resources :restaurants do
     resources :reviews
 end
+
+ resources :reviews do
+  resources :endorsements
+end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -22,7 +27,7 @@ end
   #   resources :products do
   #     member do
   #       get 'short'
-  #       post 'toggle'
+  #       post 'toggle'  
   #     end
   #
   #     collection do
